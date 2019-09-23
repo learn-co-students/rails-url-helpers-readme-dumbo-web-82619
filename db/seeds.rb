1ce751c269187579b0bbccf5ef7ab9296f139de6
Karina Macancela <karymacancela@gmail.com>
2019-09-23 16:41:27 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.destroy_all
+
+p1 = Post.create(title: "How to Code in Ruby", description: "Some description here")
+p2 = Post.create(title: "How to Code in Java", description: "Some description here")
+p3 = Post.create(title: "How to Code in JavaScript", description: "Some description here")
+p4 = Post.create(title: "How to Code in Swift", description: "Some description here")
+p5 = Post.create(title: "How to Code in Python", description: "Some description here")
+
+puts "It has been seeded!"
